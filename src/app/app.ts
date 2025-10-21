43e68e8a90b282623fdd3a44f5ce99e0e32561e0
Elizabetttz <karenelizabet2828@gmail.com>
2025-10-21 15:11:58 -0500

    Parte elizabet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuNav } from './menu-nav/menu-nav';
-
+import { Home } from './home/home';
+import { AboutUs } from './about-us/about-us';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuNav],
+  imports: [RouterOutlet, MenuNav, Home, AboutUs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
